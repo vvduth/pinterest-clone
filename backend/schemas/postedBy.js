eef73efcb1553139e9465587d41d85871b63767e
Duc Thai <ducthai060501@gmail.com>
2022-09-26 13:21:41 +0300

    define schemas

---
+++ backend/schemas/postedBy.js
@@ -0,0 +1,8 @@
+export default {
+    name: 'PostedBy', 
+    title: 'pstedBy', 
+    type: 'reference',
+    to: [{
+        type: 'user'
+    }]
+}
--- backend/schemas/schema.js
+++ backend/schemas/schema.js
@@ -3,7 +3,11 @@ import createSchema from 'part:@sanity/base/schema-creator'
 
 // Then import schema types from any plugins that might expose them
 import schemaTypes from 'all:part:@sanity/base/schema-type'
-
+import user from './user'
+import pin from './pin'
+import comment  from './comment'
+import save from './save'
+import postedBy from './postedBy'
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
   // We name our schema
@@ -12,5 +16,6 @@ export default createSchema({
   // to the ones provided by any plugins that are installed
   types: schemaTypes.concat([
     /* Your types here! */
+    user, pin , comment, postedBy, save
   ]),
 })
